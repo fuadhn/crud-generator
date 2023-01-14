62617dbb65e5e8967bf9b1ea1aca6c1f9b1dcb52
fuadhn <fhadinugroho@gmail.com>
2023-01-14 22:32:04 +0700

    Membuat tampilan default

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,18 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $output = [
+            'css_files' => array(),
+            'title' => 'Demo KUCRUD',
+            'subtitle' => 'KUCRUD is a free no-code PHP CodeIgniter CRUD generator from database.',
+            'output' => NULL,
+            'js_files' => array(),
+        ];
+
+        return $this->_output($output);
+    }
+
+    private function _output($output=null) {
+        return view('default/output', (array) $output);
     }
 }
