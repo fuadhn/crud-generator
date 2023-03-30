71445014045ed3f63857eda79aa00fe3189f19d0
fuadhn <fhadinugroho@gmail.com>
2023-03-30 11:05:51 +0700

    Reset configuration

--- app/Config/App.php
+++ app/Config/App.php
@@ -22,7 +22,7 @@ class App extends BaseConfig
      * explicitly and never rely on auto-guessing, especially in production
      * environments.
      */
-    public string $baseURL = 'http://localhost:81/crud-generator/';
+    public string $baseURL = 'BASE URL'; // http://localhost/crud-generator/
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
-        'password' => '',
-        'database' => '',
+        'username' => 'USERNAME',
+        'password' => 'PASSWORD',
+        'database' => 'DATABASE',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
